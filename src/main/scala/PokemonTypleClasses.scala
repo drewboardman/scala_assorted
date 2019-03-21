059481fe163d2c3ef53226216f7fc102d2b2ba0b
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-20 21:17:26 -0500

    trying to implement Ordered

--- src/main/scala/PokemonTypleClasses.scala
+++ src/main/scala/PokemonTypleClasses.scala
@@ -1,7 +1,11 @@
-trait PokeMove
+sealed trait PokeMove {
+  def show: String
+}
 
 trait FireMove extends PokeMove
-case object Ember extends FireMove
+case object Ember extends FireMove {
+  override def show = "Vinewhip"
+}
 case object FlameThrower extends FireMove {
   override def show = "FlameThrower"
 }
@@ -10,7 +14,9 @@ case object FireBlast extends FireMove {
 }
 
 trait WaterMove extends PokeMove
-case object Bubble extends WaterMove
+case object Bubble extends WaterMove {
+  override def show = "Bubble"
+}
 case object WaterGun extends WaterMove {
   override def show = "Watergun"
 }
@@ -20,26 +26,44 @@ case object VineWhip extends GrassMove {
   override def show = "Vinewhip"
 }
 
-trait Pokemon {
-  val name: String
-  val move: PokeMove
+sealed trait Pokemon
+sealed trait Tier1 extends Pokemon
+implicit val Tier1Ord = new Ordered[Tier1] // why can't i create an instance of this?
+
+sealed trait Tier2 extends Pokemon
+sealed trait Tier3 extends Pokemon
+
+trait FirePokemon extends Pokemon
+case object Charmander extends FirePokemon with Tier1
+case object Charmeleon extends FirePokemon with Tier3
+case object Charizard extends FirePokemon with Tier3
+
+trait WaterPokemon extends Pokemon
+case object Squirtle extends WaterPokemon with Tier1
+case object WarTortle extends WaterPokemon with Tier2
+case object Blastoise extends WaterPokemon with Tier3
+
+trait GrassPokemon extends Pokemon
+case object Bulbasaur extends GrassPokemon with Tier1
+case object Ivysaur extends GrassPokemon with Tier2
+case object Venusaur extends GrassPokemon with Tier3
+
+trait Battle[A <: Pokemon] {
+  def ability(a: A): PokeMove
+  def fight[B <: Pokemon](a1: A, a2: B): Pokemon
 }
 
-case class FirePokemon(override val name: String, override val move: FireMove) extends Pokemon
-val charmander = FirePokemon("Charmander", Ember)
-val charmeleon = FirePokemon("Charmeleon", FlameThrower)
-val charizard = FirePokemon("Charizard", FireBlast)
+implicit class PokeOps(a: Pokemon) {
+  def considerTier(a: Pokemon, b: Pokemon): Pokemon =
+}
+implicit val FirePokemonBattle = new Battle[FirePokemon] {
+  override def ability(a: FirePokemon): PokeMove = a match {
+    case Charmander => Ember
+    case Charmeleon => FlameThrower
+    case Charizard => FireBlast
+  }
 
-case class WaterPokemon(override val name: String, override val move: WaterMove) extends Pokemon
-val squirtle = WaterPokemon("Squirtle", Bubble)
-val wartortle = WaterPokemon("Wartortle", WaterGun)
-val blastoise = WaterPokemon("Blastoise", WaterGun)
-
-case class GrassPokemon(override val name: String, override val move: GrassMove) extends Pokemon
-val bulbasaur = GrassPokemon("Bulbasaur", VineWhip)
-val ivysaur = GrassPokemon("Ivysaur", VineWhip)
-val venusaur = GrassPokemon("Venusaur", VineWhip)
-
-object Battle {
-  def movePicker(pokemon: Pokemon) = ???
+  override def fight(a: FirePokemon, b: Pokemon): Pokemon = b match {
+    case w: WaterPokemon => w
+  }
 }
